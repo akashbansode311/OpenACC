afc21b0046421efb83ac1c1f1e5b8ecff0f80fce
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2026-04-21 01:26:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@
 #SBATCH --time=00:05:00
 #SBATCH --output=out_%j.txt
 #SBATCH --error=err_%j.txt
-
+#SBATCH --reservation=cdac-app
 ulimit -s unlimited
 # ---- Load modules ----
 ml spack
-. /home/apps/SPACK/spack/share/spack/setup-env.sh
+. /home/apps/spack/share/spack/setup-env.sh
 
-spack load nvhpc@25.5
+spack load nvhpc@23.11/cc6dko5
 
 nvc++ --version
 
@@ -21,4 +21,4 @@ nvc++ --version
 # ---- Run ----
 cd $HOME/OpenACC
 
-time ./filename
+time ./06.matrix_mult_parallel
