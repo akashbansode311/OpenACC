9db4f45ce3301859af8a5befa9a5a33a1967efd9
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2026-07-07 19:53:04 +0530

    Update 02.vector_addition_parallel.cpp

--- 02.vector_addition_parallel.cpp
+++ 02.vector_addition_parallel.cpp
@@ -5,7 +5,7 @@ int main()
 {
         const int N = 1000000; // Vector Size
 
-        // Declaration of the vector arrays
+        // Declaration of the vectors
         float a[N], b[N], c[N];
 
         // Initialize the input vectors
@@ -17,15 +17,22 @@ int main()
 
         // OpenACC Parallel Region
         // The "data" directive manages data transfer between CPU (Host) and GPU (Device)
+        // Copy input arrays to GPU and output array back to CPU
         #pragma acc data copyin(a[0:N], b[0:N]) copyout(c[0:N])
         {
+                // Execute loop on GPU
                 #pragma acc parallel loop
                 for (int i = 0 ; i < N ; ++i)
                 {
                         c[i] = a[i] + b[i];
                 }
         }
+        std::cout << "First 10 Elements of Result:\n";
 
+        for (int i = 0; i < 10; i++)
+        {
+                std::cout << "c[" << i << "] = " << c[i] << std::endl;
+        }
         std::cout << "[SUCCESS] Vector Addition completed...\n";
         return 0;
 }
